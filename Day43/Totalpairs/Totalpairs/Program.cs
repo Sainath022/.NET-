f875452f2f489df716175cc6c5655ff61b64d076
Sainath <120022709+Sainath022@users.noreply.github.com>
2023-05-31 12:18:53 +0530

    Add files via upload

---
+++ Day43/Totalpairs/Totalpairs/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Totalpairs
+{
+    class Program
+
+    {
+        static long countPairs(int[] arr)
+
+        {
+
+            int n = arr.Length;
+
+            long pairCount = 0;
+
+            int[] frequency = new int[101];
+
+            for (int i = 0; i < n; i++)
+
+            {
+
+                frequency[arr[i]]++;
+
+            }
+
+            for (int i = 0; i < n; i++)
+
+            {
+
+                pairCount += frequency[arr[i]] - 1;
+
+                frequency[arr[i]] = 0;
+
+            }
+            return pairCount;
+
+        }
+
+        static void Main(string[] args)
+
+        {
+
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), Convert.ToInt32);
+
+
+
+            long result = countPairs(arr);
+
+            Console.WriteLine(result);
+
+        }
+
+    }
+}
---
+++ Day43/birthdaycake/birthdaycake/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace birthdaycake
+{
+    class Program
+
+    {
+        static int birthdayCakeCandles(int[] candles)
+
+        {
+
+            int maxHeight = 0;
+
+            int count = 0;
+
+
+
+            foreach (int height in candles)
+
+            {
+
+                if (height > maxHeight)
+
+                {
+
+                    maxHeight = height;
+
+                    count = 1;
+
+                }
+
+                else if (height == maxHeight)
+
+                {
+
+                    count++;
+
+                }
+
+            }
+
+
+
+            return count;
+
+        }
+        static void Main(string[] args)
+
+        {
+
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int[] candles = Array.ConvertAll(Console.ReadLine().Split(' '), Convert.ToInt32);
+
+
+
+            int result = birthdayCakeCandles(candles);
+
+            Console.WriteLine(result);
+
+        }
+
+    }
+}
