184da122f7fca08f961e1519fb65cad7c9a5ff88
Sainath <120022709+Sainath022@users.noreply.github.com>
2023-06-06 09:34:14 +0530

    Add files via upload

---
+++ Day48/endoffile/endoffile/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace endoffile
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+
+        {
+
+            string line;
+
+            int lineNumber = 1;
+
+
+
+            while ((line = Console.ReadLine()) != null)
+
+            {
+
+                Console.WriteLine(lineNumber + " " + line);
+
+                lineNumber++;
+
+            }
+
+        }
+
+    }
+
+
+}
+
