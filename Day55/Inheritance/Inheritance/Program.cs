8c45fec56ea6487f2cb5de073b8d291921dd0d67
Sainath <120022709+Sainath022@users.noreply.github.com>
2023-06-12 09:56:03 +0530

    Add files via upload

---
+++ Day55/Inheritance/Inheritance/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    internal class Program
+    {
+        class Animal
+
+        {
+
+            public void walk()
+
+            {
+
+                Console.WriteLine("I am walking");
+
+            }
+
+        }
+
+
+
+        class Bird : Animal
+
+        {
+
+            public void fly()
+
+            {
+
+                Console.WriteLine("I am flying");
+
+            }
+
+
+
+            public void sing()
+
+            {
+
+                Console.WriteLine("I am singing");
+
+            }
+
+        }
+        class Solution
+
+        {
+
+            public static void Main(string[] args)
+
+            {
+
+                Bird bird = new Bird();
+
+
+
+                bird.walk();
+
+                bird.fly();
+
+                bird.sing();
+
+            }
+
+        }
+    }
+}
